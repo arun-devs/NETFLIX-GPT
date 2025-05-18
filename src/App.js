a3d5e8c40fec942112a269b4b65f025d9656f506
arun dev <arundevarajan346@gmail.com>
2025-05-18 11:15:03 +0530

    home

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import "./App.css";
+import Body from "./Components/Body";
 
 function App() {
   return (
-    <div className="align-bottom bg-red-600">
-      namaste everyone
-      <button className=" w-4 p-4 bg-gray-100 round"></button>
-    </div>
+    <Body/>
   );
 }
 
